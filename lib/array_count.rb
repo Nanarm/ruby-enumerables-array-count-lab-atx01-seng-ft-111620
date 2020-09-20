72d910e7473077202a37dbc9f95979f2d3a375da
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 02:59:54 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,18 +1,14 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |element|
-    if element.String?
-      element += 1 
-    end
+    element.String?
    end 
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |emp|
-     if emp.empty?
-       emp += 1
-    end 
-    return emp
+      emp.empty?
+     
   end
 end
