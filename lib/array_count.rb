65535d7eb09f5a994c7de6bd4519c7418d8a060e
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 03:16:13 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,7 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  array.count { |string| string.to_index.empty? }
+  array.count { |string| string.empty? }
      
 
 end
